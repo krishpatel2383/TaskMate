c2316f14dfa58dbea09cf61123d85cab306c2f66
krishpatel2383 <krishpatel2383@gmail.com>
2023-07-05 13:38:40 +0530

    configured email serivce

--- backend/TaskMate/src/main/java/com/krish/TaskMate/Controller/UserController.java
+++ backend/TaskMate/src/main/java/com/krish/TaskMate/Controller/UserController.java
@@ -1,5 +1,7 @@
 package com.krish.TaskMate.Controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.krish.TaskMate.Model.ResetPasswordRequest;
 import com.krish.TaskMate.Model.User;
-import com.krish.TaskMate.Service.EmailService;
 import com.krish.TaskMate.Service.UserService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.validation.Valid;
 
 @RestController
@@ -26,9 +35,6 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-
-	@Autowired
-	private EmailService emailService;
 
 	@GetMapping("/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable Long id) {
@@ -62,9 +68,31 @@ public class UserController {
 
 	@PostMapping("/forgot-password")
 	public ResponseEntity<?> forgotPassword(@RequestBody User user) {
+		Dotenv dotenv = Dotenv.load();
+		String sendgridApiKey = dotenv.get("SENDGRID_API_KEY");
 		String resetToken = userService.generateResetToken(user.getEmail());
-		emailService.sendResetPasswordEmail(user.getEmail(), resetToken);
-		return ResponseEntity.ok().build();
+		String fromEmail = "krish.taskmate@gmail.com";
+		String toEmail = user.getEmail();
+		String subject = "Reset Password";
+		String message = "Please use the following link to reset your account password:\n http://localhost:3000/"
+				+ user.getEmail() + "/reset-password?token=" + resetToken;
+
+		Email from = new Email(fromEmail);
+		Email to = new Email(toEmail);
+		Content content = new Content("text/plain", message);
+		Mail mail = new Mail(from, subject, to, content);
+		SendGrid sg = new SendGrid(sendgridApiKey);
+		Request request = new Request();
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			return ResponseEntity.ok().build();
+		} catch (IOException ex) {
+			// Handle SendGrid exception
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@PostMapping("/reset-password")
